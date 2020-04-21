ca4eb503646f4e986754c162715f4a7d1dc7a48d
xuzhidong <xuzhidong@qianxin.com>
2020-04-21 17:06:06 +0800

    init

---
+++ controller/Index.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Created by phpstorm.
+ * User: xzd
+ * Date: 2020/4/21
+ * Time: 17:03
+ */
+
+namespace app\permission\controller;
+
+
+use app\BaseController;
+
+class Index extends BaseController
+{
+    public function index()
+    {
+        echo 'this is submodule -- permission';
+    }
+}
---
+++ service/User.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Created by phpstorm.
+ * User: xzd
+ * Date: 2020/4/21
+ * Time: 17:05
+ */
+
+namespace app\permission\service;
+
+
+class User
+{
+
+}
